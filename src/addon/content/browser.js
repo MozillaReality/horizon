c320e32b4deb691bed44bb5c60dd979b2606c0c0
Kevin Grandon <kevingrandon@yahoo.com>
2015-04-21 17:41:24 -0700

    Try granting permissions to browser from addon.

--- src/addon/content/browser.js
+++ src/addon/content/browser.js
@@ -1,14 +1,38 @@
+Cu.import('resource://gre/modules/Services.jsm');
+
+var origin = 'http://kevingrandon.github.io';
+
 var vrbrowser = function () {
   return {
     init: function () {
       gBrowser.addEventListener("load", function () {
-        dump('GOT LOAD FROM EXTENSION.');
       });
     },
 
     run: function () {
       dump('Trying to run() from extension.');
-      gBrowser.selectedTab = gBrowser.addTab('http://kevingrandon.github.io/browser/web/');
+      var newTabBrowser = gBrowser.addTab(origin + '/browser/web/');
+      gBrowser.selectedTab = newTabBrowser;
+
+      var browser = gBrowser.getBrowserForTab(newTabBrowser);
+      newTabBrowser.addEventListener('load', () => {
+        console.log('Got load event.');
+        var aWindow = browser.contentWindowAsCPOW;
+        if (!aWindow) {
+          return;
+        }
+        var docShell = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+                            .getInterface(Ci.nsIDocShell);
+        console.log('Got docShell. App id is:', docShell.appId);
+        docShell.setIsApp(1);
+        docShell.setIsBrowserInsideApp(1);
+        console.log('Called setIsApp. App id: ', docShell.appId);
+
+        var uri = Services.io.newURI(origin, null, null);
+        Services.perms.add(uri, 'embed-apps', Services.perms.ALLOW_ACTION);
+        Services.perms.add(uri, 'browser', Services.perms.ALLOW_ACTION);
+        Services.perms.add(uri, 'systemXHR', Services.perms.ALLOW_ACTION);
+      });
     }
   };
 }();
