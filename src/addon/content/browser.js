a136f9eea118efdcc232d7a95cf4f516f49431aa
Kevin Grandon <kevingrandon@yahoo.com>
2015-04-22 13:42:18 -0700

    Try installing app as a 'system' app.

--- src/addon/content/browser.js
+++ src/addon/content/browser.js
@@ -1,6 +1,91 @@
 Cu.import('resource://gre/modules/Services.jsm');
+Cu.import("resource://gre/modules/Webapps.jsm");
+Cu.import("resource://gre/modules/AppsUtils.jsm");
 
 var origin = 'http://kevingrandon.github.io';
+
+var BrowserAppInstaller = {
+
+  manifestUrl: null,
+
+  /**
+   * Resolves to a json manifest.
+   */
+  loadManifest: function() {
+    return new Promise((aResolve, aReject) => {
+      console.log("Loading manifest " + this.manifestUrl);
+
+      let xhr =  Cc["@mozilla.org/xmlextras/xmlhttprequest;1"]
+                 .createInstance(Ci.nsIXMLHttpRequest);
+      xhr.mozBackgroundRequest = true;
+      xhr.open("GET", this.manifestUrl);
+      xhr.responseType = "json";
+      xhr.addEventListener("load", () => {
+        if (xhr.status >= 200 && xhr.status < 400) {
+          console.log("Success loading " + this.manifestUrl);
+          aResolve(xhr.response);
+        } else {
+          aReject("Error loading " + this.manifestUrl);
+        }
+      });
+      xhr.addEventListener("error", () => {
+        aReject("Error loading " + this.manifestUrl);
+      });
+      xhr.send(null);
+    });
+  },
+
+  /**
+   * Installs the system app into the registry.
+   */
+  installSystemApp: function(aManifest) {
+    // Get the appropriate startup url from the manifest launch_path.
+    let base = Services.io.newURI(this.manifestUrl, null, null);
+    let origin = base.prePath;
+    let helper = new ManifestHelper(aManifest, origin, this.manifestUrl);
+    this.startupUrl = helper.fullLaunchPath();
+    console.log('Startup url is:' + this.startupUrl);
+
+    return new Promise((aResolve, aReject) => {
+      console.log("Origin is " + origin);
+      let appData = {
+        app: {
+          installOrigin: origin,
+          origin: origin,
+          manifest: aManifest,
+          manifestURL: this.manifestUrl,
+          manifestHash: AppsUtils.computeHash(JSON.stringify(aManifest)),
+          appStatus: Ci.nsIPrincipal.APP_STATUS_CERTIFIED
+        },
+        appId: 1,
+        isBrowser: false,
+        isPackage: false
+      };
+
+      DOMApplicationRegistry.confirmInstall(appData, null, aResolve);
+    });
+  },
+
+  configure: function() {
+    Services.prefs.setCharPref("b2g.system_manifest_url", this.manifestUrl);
+    Services.prefs.setCharPref("b2g.system_startup_url", this.startupUrl);
+    return Promise.resolve();
+  },
+
+  /**
+   * Installs the browser as a system app.
+   */
+  install: function(manifestUrl) {
+    this.manifestUrl = manifestUrl;
+    return DOMApplicationRegistry.registryReady
+          .then(this.loadManifest.bind(this))
+          .then(this.installSystemApp.bind(this))
+          .then(this.configure.bind(this))
+          .catch(e => {
+            console.log('Got error when installing the vr browser.', e)
+          });
+  }
+};
 
 /**
  * Installs the toolbar button with the given ID into the given
@@ -38,8 +123,7 @@ var vrbrowser = function () {
       installButton('nav-bar', 'vrbrowser-toolbar-button');
       installButton('addon-bar', 'vrbrowser-toolbar-button');
 
-      gBrowser.addEventListener("load", function () {
-      });
+      BrowserAppInstaller.install(origin + '/browser/web/manifest.webapp');
     },
 
     run: function () {
@@ -54,6 +138,8 @@ var vrbrowser = function () {
         if (!aWindow) {
           return;
         }
+
+        /*
         var docShell = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
                             .getInterface(Ci.nsIDocShell);
         console.log('Got docShell. App id is:', docShell.appId);
@@ -65,6 +151,7 @@ var vrbrowser = function () {
         Services.perms.add(uri, 'embed-apps', Services.perms.ALLOW_ACTION);
         Services.perms.add(uri, 'browser', Services.perms.ALLOW_ACTION);
         Services.perms.add(uri, 'systemXHR', Services.perms.ALLOW_ACTION);
+        */
       });
     }
   };
