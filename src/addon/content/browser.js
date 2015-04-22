6e32d691a7b1d033de018037d67957c3784c2ae6
Kevin Grandon <kevingrandon@yahoo.com>
2015-04-21 19:59:36 -0700

    Install VR button onto toolbar.

--- src/addon/content/browser.js
+++ src/addon/content/browser.js
@@ -2,9 +2,42 @@ Cu.import('resource://gre/modules/Services.jsm');
 
 var origin = 'http://kevingrandon.github.io';
 
+/**
+ * Installs the toolbar button with the given ID into the given
+ * toolbar, if it is not already present in the document.
+ *
+ * @param {string} toolbarId The ID of the toolbar to install to.
+ * @param {string} id The ID of the button to install.
+ * @param {string} afterId The ID of the element to insert after. @optional
+ */
+function installButton(toolbarId, id, afterId) {
+  if (!document.getElementById(id)) {
+      var toolbar = document.getElementById(toolbarId);
+
+      // If no afterId is given, then append the item to the toolbar
+      var before = null;
+      if (afterId) {
+          let elem = document.getElementById(afterId);
+          if (elem && elem.parentNode == toolbar)
+              before = elem.nextElementSibling;
+      }
+
+      toolbar.insertItem(id, before);
+      toolbar.setAttribute("currentset", toolbar.currentSet);
+      document.persist(toolbar.id, "currentset");
+
+      if (toolbarId == "addon-bar")
+          toolbar.collapsed = false;
+  }
+}
+
 var vrbrowser = function () {
   return {
     init: function () {
+      // Install the icon onto the main toolbar.
+      installButton('nav-bar', 'vrbrowser-toolbar-button');
+      installButton('addon-bar', 'vrbrowser-toolbar-button');
+
       gBrowser.addEventListener("load", function () {
       });
     },
@@ -25,7 +58,7 @@ var vrbrowser = function () {
                             .getInterface(Ci.nsIDocShell);
         console.log('Got docShell. App id is:', docShell.appId);
         docShell.setIsApp(1);
-        docShell.setIsBrowserInsideApp(1);
+        //docShell.setIsBrowserInsideApp(1);
         console.log('Called setIsApp. App id: ', docShell.appId);
 
         var uri = Services.io.newURI(origin, null, null);
