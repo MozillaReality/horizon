042896ad228c37752d894b0c50727eb4c72f8ba0
Casey Yee <caseyyee.ca@gmail.com>
2015-05-02 08:39:18 -0700

    Viewport manager

---
+++ src/web/js/viewport_manager.js
@@ -0,0 +1,87 @@
+/*
+global HMDVRDevice, PositionSensorVRDevice
+*/
+
+export default class ViewportManager {
+  constructor() {
+    this.vrDevices = {};
+
+    this.enter = $('#entervr');
+    this.container = $('#frames');
+
+    this.getVrDevices().then(devices => {
+      this.vrDevices = devices;
+    });
+  }
+
+  filterInvalidDevices(devices) {
+    let oculusDevices = devices.filter((device) => {
+      return device.deviceName.toLowerCase().indexOf('oculus') !== -1;
+    });
+
+    if (oculusDevices.length >= 1) {
+      return devices.filter((device) => {
+        return device.deviceName.toLowerCase().indexOf('cardboard') === -1;
+      });
+    } else {
+      return devices;
+    }
+  }
+
+  vrDeviceCallback(devices) {
+    devices = this.filterInvalidDevices(devices);
+
+    let headset;
+    let position;
+
+    for (let i = 0; i < devices.length; i++) {
+      let device = devices[i];
+      if (device instanceof HMDVRDevice) {
+        headset = device;
+      }
+      if (device instanceof PositionSensorVRDevice) {
+        position = device;
+      }
+      if (position && headset) {
+        return ({
+          headset: headset,
+          position: position
+        });
+      }
+    }
+
+    return false;
+  }
+
+  getVrDevices() {
+    return new Promise((resolve, reject) => {
+      if (navigator.getVRDevices) {
+        navigator.getVRDevices().then(devices => {
+          resolve(this.vrDeviceCallback(devices));
+        });
+      } else {
+        reject('No VR devices');
+      }
+    });
+  }
+
+  launchFs(element, opts) {
+    if (element.requestFullscreen) {
+      element.requestFullscreen(opts);
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen(opts);
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen(opts);
+    }
+  }
+
+  enterVr() {
+    this.launchFs(this.container, {
+      vrDisplay: this.vrDevices.headset
+    });
+  }
+
+  start() {
+    this.enter.addEventListener('click', this.enterVr.bind(this));
+  }
+}
