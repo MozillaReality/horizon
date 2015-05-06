44089be398fbcdcf05dc5a7650c6525c61306e93
Kevin Grandon <kevingrandon@yahoo.com>
2015-05-05 22:13:59 -0700

    Do not allow keyboard commands when focused on a field. Fixes #14.

--- src/web/js/controls/keyboard.js
+++ src/web/js/controls/keyboard.js
@@ -1,3 +1,12 @@
+/**
+ * A list of selectors for which we ignore keyboard events for.
+ */
+const FOCUSABLE_ELEMENTS = [
+  'a[href]', 'area[href]', 'input:not([disabled]):not([readonly])',
+  'select:not([disabled]):not([readonly])', 'textarea:not([disabled]):not([readonly])',
+  'button:not([disabled]):not([readonly])', 'iframe', 'object', 'embed',
+  '[tabindex]:not([tabindex="-1"])', '[contenteditable]'];
+
 export default class KeyboardControl {
 
   constructor() {
@@ -27,10 +36,22 @@ export default class KeyboardControl {
   }
 
   /**
+   * Checks if an event target matches one of the selectors in FOCUSABLE_ELEMENTS.
+   * @param {Event}
+   */
+  isFieldFocused(e) {
+    return (e ? e.target : document.activeElement).matches(FOCUSABLE_ELEMENTS);
+  }
+
+  /**
    * Keydown event handler.
    * @param {Event}
    */
   handleEvent(e) {
+    if (this.isFieldFocused()) {
+      return;
+    }
+
     var inputChord = this.getModifiers(e).
       concat([e.key]).
       join(' ').
