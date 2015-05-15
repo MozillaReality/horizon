14523bdf049fecd04936eada2e1b45ab1578d64e
Christopher Van <cvan@mozilla.com>
2015-05-15 00:56:35 -0700

    disable inactive back/forward browser buttons

--- src/web/js/lib/utils.js
+++ src/web/js/lib/utils.js
@@ -11,4 +11,15 @@ export default class Utils {
   $$(sel) {
     return Array.prototype.slice.call(document.querySelectorAll(sel));
   }
+
+  evaluateDOMRequest(req) {
+    return new Promise((resolve, reject) => {
+      req.onsuccess = e => {
+        resolve(e.target.result);
+      };
+      req.onerror = e => {
+        reject(e);
+      };
+    });
+  }
 }
