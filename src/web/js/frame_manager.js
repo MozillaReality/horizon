7e2f0de436086701123447f79f1dbd54529c5216
Casey Yee <caseyyee.ca@gmail.com>
2015-07-08 13:06:21 -0700

    set cursor at depth, additional content iframe mouse eventing

--- src/web/js/frame_manager.js
+++ src/web/js/frame_manager.js
@@ -46,6 +46,7 @@ export default class FrameManager {
     this.loading = $('#loading');
     this.closehudButton = $('#closehud');
     this.hudBackground = $('#background');
+    this.cursor = $('#cursor');
 
     // Element at cursor.
     this.cursorElement = null;
@@ -550,14 +551,33 @@ export default class FrameManager {
   /**
    * Cursor
    */
-  intersectCursor() {
-    var el = document.elementFromPoint(0, 0);
+  handleCursorTransition() {
+    this.cursorElement.transitioned = true;
+  }
 
-    if (el !== this.cursorElement) {
-      this.cursorMouseLeave(el);
-      this.cursorElement = el;
-      this.cursorMouseEnter();
-    }
+  intersectCursor() {
+    setTimeout(() => {
+      var el = document.elementFromPoint(0, 0);
+      if (el !== this.cursorElement || this.cursorElement.transitioned) {
+        this.cursorMouseLeave(el);
+        this.cursorElement = el;
+
+        // flag elements to have translations re-computed on animation and transition events.
+        if (!this.cursorElement.transitioned) {
+          this.cursorElement.addEventListener('transitionend', this.handleCursorTransition.bind(this));
+          this.cursorElement.addEventListener('animationend', this.handleCursorTransition.bind(this));
+        } else {
+          this.cursorElement.removeEventListener('transitionend', this.handleCursorTransition.bind(this));
+          this.cursorElement.removeEventListener('animationend', this.handleCursorTransition.bind(this));
+          delete this.cursorElement.transitioned;
+        }
+
+        this.cursorElementTranslation = this.getElementTranslation(el);
+        this.cursorMouseEnter();
+      }
+
+      requestAnimationFrame(this.intersectCursor.bind(this));
+    }, 100); // Checking for element at every RAF is unecessary, so we set a interval at a much lower frequency.
   }
 
   cursorMouseLeave(newEl) {
@@ -610,6 +630,8 @@ export default class FrameManager {
       }
       el.focus();
 
+      this.mouseIntoIframe('mousedown');
+
       this.utils.sleep(mouseConfig.formSubmitThreshold).then(() => {
         // If the click button has been depressed for a long time, assume a form submission.
         if (el === this.cursorDownElement) {
@@ -627,47 +649,136 @@ export default class FrameManager {
     return Promise.resolve();
   }
 
-  intersectIframe() {
-    var el = this.cursorElement;
-    if (el !== this.viewportManager.contentContainer) {
-      return;
+
+  /**
+   * Traverse up tree to find containing element with matrix3d transform.
+   *
+   * @param {Element} el Element
+   * @returns {Array} Array of matrix3d values.
+   */
+  getNearest3dTransform(el) {
+    if (!el) {
+      return false;
+    }
+    let transform = window.getComputedStyle(el).transform;
+    if (transform.indexOf('matrix3d') === -1) {
+      return this.getNearest3dTransform(el.parentElement);
+    } else {
+      return transform;
+    }
+  }
+
+
+  /**
+   * Return 3d translation of element.
+   *
+   * @param {Element} el Element
+   * @returns { {x:Number, y:Number, z:Number} } Translation
+   */
+  getElementTranslation(el) {
+    if (!el) {
+      return false;
     }
 
-    // Retrieve offsets from element transform matrix.
-    var transform = window.getComputedStyle(el).transform;
-    if (transform === 'none') {
-      return;
+    let transform = this.getNearest3dTransform(el);
+    if (!transform) {
+      return false;
     }
-    var cssMatrix = matrix.matrixFromCss(transform);
-    var offsetX = -cssMatrix[12], offsetY = cssMatrix[13], offsetZ = -cssMatrix[14];
+
+    let cssMatrix = matrix.matrixFromCss(transform);
+    return {
+      x: -(cssMatrix[12] * this.viewportManager.monoScale), // multiply by mono container scale
+      y: cssMatrix[13] * this.viewportManager.monoScale,
+      z: -cssMatrix[14]
+    };
+  }
+
+
+  /**
+   * Return direction from VR headset quaternion.   Use -Z as forward.
+   *
+   * @returns {Object} Direction
+   * @returns { {x:Number, y:Number, z:Number} } Direction
+   */
+  getDirection() {
+    let hmd = this.viewportManager.hmdState;
+    if (!hmd || !hmd.orientation) {
+      return false;
+    }
 
     // Transform the HMD quaternion by direction vector.
-    var hmd = this.viewportManager;
-    var direction = vec4.transformQuat([], [0, 0, -1, 0],
+    let direction = vec4.transformQuat([], [0, 0, -1, 0],
       [hmd.orientation.x, hmd.orientation.y, hmd.orientation.z, hmd.orientation.w]);
-    var dx = direction[0], dy = direction[1], dz = direction[2];
 
-    // Scale HMD position to match CSS values.
-    var cmToPixel = 96 / 2.54;
-    var pixelPerMeters = -100 * cmToPixel;
+    return {
+      x: -direction[0],
+      y: -direction[1],
+      z: direction[2]
+    };
+  }
 
-    // Apply HMD position.
-    var translateX = 0, translateY = 0, translateZ = 0;
-    if (hmd.position !== null) {
-      translateX = -hmd.position.x * pixelPerMeters;
-      translateY = -hmd.position.y * pixelPerMeters;
-      translateZ = -hmd.position.z * pixelPerMeters;
+  /**
+   * Return position offset from VR headset.
+   *
+   * @returns {Object} Position
+   * @returns { {x:Number, y:Number, z:Number} } Position
+   */
+  getPosition() {
+    let hmd = this.viewportManager.hmdState;
+    if (!hmd) {
+      return false;
     }
 
-    // Solve intersection.
-    var distance = offsetZ + translateZ;
-    var intersectionX = distance / dz * -dx + translateX + offsetX;
-    var intersectionY = distance / dz * -dy + translateY + offsetY;
-    intersectionY *= -1;
+    let scale = this.settings.pixels_per_meter * this.settings.hmd_scale;
+    if (hmd.position) {
+      return {
+        x: -hmd.position.x * scale,
+        y: -hmd.position.y * scale,
+        z: -hmd.position.z * scale
+      };
+    } else {
+      return {x: 0, y: 0, z: 0};
+    }
+  }
 
-    this.frameCommunicator.send('mouse.click', {
-      top: intersectionY,
-      left: intersectionX
+  /**
+   * Position the cursor at the same depth as the mono iframe container.
+   * Otherwise, use the depth set on cursor element.
+   */
+  positionCursor() {
+    let translation = this.cursorElementTranslation;
+    let direction = this.getDirection();
+    let position = this.getPosition();
+
+    if (translation && direction && position) {
+      // Find intersection on plane.
+      let distance = translation.z + position.z;
+      let intersectionX = distance / direction.z * direction.x + position.x + translation.x;
+      let intersectionY = -(distance / direction.z * direction.y + position.y + translation.y);
+      // Use pythagoras to find depth at intersection.
+      let intersectionZx = Math.sqrt(Math.pow(distance / direction.z * direction.x, 2) + Math.pow(distance, 2));
+      let intersectionZy = Math.sqrt(Math.pow(distance / direction.z * direction.y, 2) + Math.pow(distance, 2));
+      let intersectionZ = Math.max(intersectionZx, intersectionZy) - 10; // bias cursor depth to avoid z-fighting.
+      this.intersectionX = intersectionX;
+      this.intersectionY = intersectionY;
+
+      this.cursor.classList.add('is-visible');
+      this.cursor.style.transform = `translate3d(0, 0, -${intersectionZ}px)`;
+    } else {
+      this.cursor.classList.remove('is-visible');
+      this.cursor.style.transform = '';
+    }
+
+    requestAnimationFrame(this.positionCursor.bind(this));
+  }
+
+  mouseIntoIframe(eventName) {
+    if (this.cursorElement !== this.viewportManager.contentContainer) {
+      return;
+    }
+    this.frameCommunicator.send('mouse.' + eventName, {
+      top: this.intersectionY / this.viewportManager.monoScale,
+      left: this.intersectionX / this.viewportManager.monoScale
     });
   }
 
@@ -675,11 +786,12 @@ export default class FrameManager {
     let el = this.cursorElement;
 
     if (el) {
-      this.intersectIframe();
       this.cursorDownElement = null;
 
       this.utils.emitMouseEvent('mouseup', el);
       this.utils.emitMouseEvent('click', el);
+
+      this.mouseIntoIframe('mouseup');
     }
 
     return Promise.resolve();
@@ -716,9 +828,10 @@ export default class FrameManager {
   }
 
   init(runtime) {
+    this.frameCommunicator = runtime.frameCommunicator;
+    this.settings = runtime.settings;
     this.utils = runtime.utils;
     this.viewportManager = runtime.viewportManager;
-    this.frameCommunicator = runtime.frameCommunicator;
 
     // Preload the sound effects so we can play them later.
     Promise.all([
@@ -836,7 +949,8 @@ export default class FrameManager {
       }
     });
 
-    setInterval(this.intersectCursor.bind(this), 100);
+    requestAnimationFrame(this.intersectCursor.bind(this));
+    requestAnimationFrame(this.positionCursor.bind(this));
 
     runtime.keyboardInput.assign({
       'ctrl =': () => this.activeFrame.zoomIn(),
