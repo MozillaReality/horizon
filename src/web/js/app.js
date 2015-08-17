d99ccd51973d32d16b3b5a433f1b2d60e672365f
Kevin Grandon <kevingrandon@yahoo.com>
2015-08-16 17:22:50 -0700

    Basic integration test support.
    This adds basic integration test support using the graphene marionette runner library which is also used by browser.html. Now that we're adding more and more browser like functionality, it would be good to have a higher level testing framework to make sure we don't regress these features.

--- src/web/js/app.js
+++ src/web/js/app.js
@@ -1,4 +1,5 @@
 import Browser from './components/browser.js';
 
 var runtime = {};
+window.runtime = runtime; // Expose runtime to the window object for test hooks.
 React.render(<Browser runtime={runtime}/>, document.body);
